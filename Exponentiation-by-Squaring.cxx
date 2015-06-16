b6c7f725d00eaf85ede3fcead04a01a49690fc10
dbakti7 <dbakti1605@gmail.com>
2015-06-16 23:49:39 +0800

    Exponentiation-by-Squaring first commit

---
+++ Exponentiation-by-Squaring.cxx
@@ -0,0 +1,26 @@
+/* 
+ * Exponentiation-by-Squaring.cpp
+ * Written by: dbakti7
+ * 16 June 2015
+ * */
+#include <cstdio>
+long long power(long long a, long long b, long long m) {
+	// Returns the result of (a ^ b) modulo m
+	if(b == 0)
+		return 1;
+	if(b == 1)
+		return a;
+	if(b % 2 == 0)
+		return power((a * a) % m, b/2, m);
+	else
+		return (power((a * a) % m, b/2, m) * a) % m;
+}
+int main()
+{
+	long long a, b, m;
+	printf("Enter 3 numbers, a, b, and m to calculate (a ^ b) mod m:\n");
+	while((scanf("%lld %lld %lld", &a, &b, &m)) != EOF) {
+		printf("(a ^ b) % m = %lld\n", power(a, b, m));
+	}
+	return 0;
+}
